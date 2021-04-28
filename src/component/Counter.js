a762b3bdb2918d602246f9692da380e36baea20b
devlearning9874 <devlearning9874@gmail.com>
2021-04-28 16:07:46 +0530

    First commit

---
+++ src/component/Counter.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const Counter = () => {
+     return (
+         <div>
+             <h1>This is Movie List.</h1>
+         </div>
+     )
+ }
+ 
+ export default Counter
+ 
